2c89da80758d6e2b9f8d7909d985d80c0c6442de
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-09-01 08:18:19 +0300

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <AppHeader />
-  <router-view/>
+  <AppHeader/>
+  <router-view style="margin-top: 80px"/>
 </template>
 
 <script setup lang='ts'>
 import AppHeader from '@/components/AppHeader.vue'
-
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '@/assets/style';
 </style>
